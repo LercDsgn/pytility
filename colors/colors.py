e61354aaae5cf415f81908444201104313724d9a
LercDsgn <75680333+LercDsgn@users.noreply.github.com>
2022-01-23 11:14:22 +0300

    Update colors.py

--- colors/colors.py
+++ colors/colors.py
@@ -1,56 +1,33 @@
-def mixColors(*colors: tuple):
-    res = [0, 0, 0]
+def mix(*colors: tuple) -> tuple:
+    """Mix given amount of colors."""
+    res = [0]*3
     for color in colors:
-        for index, param in enumerate(color):
-            res[index] += param
+        for index, param in enumerate(color):   
+            if 0 > res[index]+param > 255: 
+                res[index] += param
     return tuple(res)
 
 
-def subtractColors(*colors: tuple):
-    if "DEBUG" in colors: 
-        ind = colors.index("DEBUG")
-        print("Found debugging notice in element", ind+1, "( index", ind, "), initializing debugger")
-        actualcolors = colors[:ind] + colors[ind+1:]
-        print("              [DEBUGGING STARTED]")
-        print("Running on color group:", actualcolors, "\n")
-        res = list(actualcolors[0])
-        for color in actualcolors[2:]:
-            if type(color)==tuple and len(color)==3:
-                print("Color: ", color) # DEBUG
-                for index, param in enumerate(color):
-                    if type(param)==int:
-                        print(f"Index {index} => {param}") # DEBUG
-                        print(res[index], "-", param) # DEBUG
-                        res[index] -= param
-                        print("      Current result is", res) # DEBUG
-                print("   ----------  ") # DEBUG
-        print("              [ DEBUGGING ENDED WITH RESULT",tuple(res),"]")
-        return tuple(res)
 
-    else:
-        res = list(colors[0])
-        for color in colors[1:]:
-            if type(color)==tuple and len(color)==3:
-                for index, param in enumerate(color):
-                    if type(param)==int:
-                        print(type(param))
-                        res[index] -= param
-        return tuple(res)
+def subtract(*colors: tuple) -> tuple:
+    """Subtract given amount of colors."""
+    res = list(colors[0])
+    for color in colors[1:]:
+        if type(color) == tuple and len(color) == 3:
+            for index, param in enumerate(color):
+                if type(param) == int and 0 < res[index] < 255:
+                    res[index] -= param
+    return tuple(res)
 
-RED =  (255,   0,    0 )
-BLUE = ( 0,    0,   255)
-LIME = ( 0,   255,   0 )           
-WHITE = mixColors(RED, BLUE, LIME)
-BLACK = subtractColors(WHITE, "DEBUG", RED, BLUE, LIME)
-print(BLACK)
+
 
 def generateColor():
     import random
-    return tuple([random.randint(0,255) for i in range(3)])
+    return tuple([random.randint(0, 255) for i in range(3)])
+
+# EXAMPLE
+# r,g,b = (255,0,0), (0,255,0), (0,0,255)
+# subract(mix(r,g,b),r,g,b) # (0,0,0)
 
 
-
-#todo: hex/hsl etc. support
-#debug for mixcolors
-#averagecolor function
 
